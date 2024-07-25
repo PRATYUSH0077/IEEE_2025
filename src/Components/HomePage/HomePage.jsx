edc9e323f55eae5eac4bdd5b87d7126d2727f536
Ankit Mewada <ipsabadoniya@gmail.comgit statusecho>
2024-07-25 14:35:47 +0530

    change size

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -5,13 +5,13 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import gwaliorImage from '../../assets/clg.jpg'; // Adjust these paths to your actual images
 import mpImage from '../../assets/fort.jpg';
-import indiaImage from '../../assets/fort2.jpg';
+// import indiaImage from '../../assets/fort2.jpg';
 import IEEELOGO  from '../../assets/IEEELOGO.png'; // Adjust the path to your MP logo image
 import ConferenceDetails from './ConferenceDetails';
 import ObjectiveComponent from './ObjectiveComponent';
 import ImportantDates from './ImportantDates';
 import ContactUs from './ContactUs';
-
+// we are start home here
 const HomePage = () => {
   const settings = {
     dots: true,
@@ -78,12 +78,12 @@ const HomePage = () => {
               <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <Image src={IEEELOGO} alt="MP Logo" boxSize={isMobile ? '40px' : '80px'} />
               </div>
-              <Text fontSize={isMobile ? 'sm' : 'lg'} mt={2} fontWeight="bold">
+              <Text fontSize={isMobile ? 'sm' : 'xl'} mt={2} fontWeight="bold">
                 Being Jointly organized by:
               </Text>
               <Box textAlign="center" mt={2}>
                 {organizingInstitutions.map((inst, index) => (
-                  <Text key={index} fontSize={isMobile ? 'sm' : 'md'}>{inst}</Text>
+                  <Text key={index} fontSize={isMobile ? 'sm' : 'lg'}>{inst}</Text>
                 ))}
               </Box>
             </Box>
