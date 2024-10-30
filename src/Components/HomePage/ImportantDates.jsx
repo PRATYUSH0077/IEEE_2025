ab5c878680c9eb2a56b6bb7ff35da071b46262f2
Ankit Mewada <ipsabadoniya@gmail.comgit statusecho>
2024-10-31 00:12:19 +0530

    extend rolling paper date

--- src/Components/HomePage/ImportantDates.jsx
+++ src/Components/HomePage/ImportantDates.jsx
@@ -4,14 +4,18 @@ import './ImportantDates.css'; // Import the CSS file for styling
 const ImportantDates = () => {
   const dates = [
     { date: '17th September, 2024', event: 'Paper submission opens' },
-    { date: '31st October, 2024', event: 'Rolling submission closes' },
+    {
+      date: '31st October, 2024',
+      newDate: '25th November, 2024',
+      event: 'Rolling submission closes'
+    },
     { date: '31st January, 2024', event: 'Acceptance of selected papers' },
     { date: '31st January, 2025', event: 'Final Paper Submission and Registration Deadline' },
     { date: '21-23 February, 2025', event: 'Conference Dates' },
   ];
 
   return (
-    <div className="important-dates-section" >
+    <div className="important-dates-section">
       <section className="dates-background">
         <div className="container">
           <div className="row" style={{ boxShadow: '0px 0px 16px 0px #757575b3', padding: '10px', borderRadius: '20px', background: 'white' }}>
@@ -21,8 +25,15 @@ const ImportantDates = () => {
                 {dates.map((item, index) => (
                   <li key={index} className="date-item">
                     <span className="event">{item.event}</span>
-                    <span className="date">{item.date}</span>
-
+                    <span className="date">
+                      {item.newDate ? (
+                        <>
+                          <span className="red-strikethrough">{item.date}</span> ➔ <span>{item.newDate}</span>
+                        </>
+                      ) : (
+                        item.date
+                      )}
+                    </span>
                   </li>
                 ))}
               </ul>
